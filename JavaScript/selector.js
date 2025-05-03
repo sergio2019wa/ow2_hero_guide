13305058c320dc546a016099be4167fb6b1cd679
Sergio <Sergio2019wa@gmail.com>
2025-05-03 13:31:44 -0700

    Attempting SQL

--- JavaScript/selector.js
+++ JavaScript/selector.js
@@ -36,37 +36,55 @@ document.addEventListener('DOMContentLoaded', function() {
   }
 });
 
+const _counter = new Map ([
+  ["Tank", new Set[""]],
+  ["Damage", new Set[""]],
+  ["Support", new Set[""]]
+])
+
+const _counter = new Map ([
+  ["Tank", new Set[""]],
+  ["Damage", new Set[""]],
+  ["Support", new Set[""]]
+])
+
+const _counter = new Map ([
+  ["Tank", new Set[""]],
+  ["Damage", new Set[""]],
+  ["Support", new Set[""]]
+])
+
 const maps_tank = new Map ([
-  ["Antarctic Peninsula" , new Set["D.va", ""]],
-  ["Blizzard World", new Set[""]],
-  ["Busan", new Set[""]],
-  ["Circuit Royal", new Set[""]],
-  ["Colosseo", new Set[""]],
-  ["Dorado", new Set[""]],
-  ["Eichenwalde", new Set[""]],
+  ["Antarctic Peninsula" , new Set["Mauga", "Junker Queen", "Orisa", "Ramattra"]],
+  ["Blizzard World", new Set["D.va", "Doomfist"]],
+  ["Busan", new Set["Orisa"]],
+  ["Circuit Royal", new Set["Doomfist", "Sigma", "Ramattra"]],
+  ["Colosseo", new Set["Ramattra", "Wrecking Ball"]],
+  ["Dorado", new Set["D.Va", "Winston"]],
+  ["Eichenwalde", new Set["D.va"]],
   ["Esperança", new Set[""]],
   ["Hanaoka", new Set[""]],
   ["Havana", new Set[""]],
-  ["Hollywood", new Set[""]],
-  ["Illios", new Set[""]],
-  ["Junkertown", new Set[""]],
-  ["King's Row", new Set[""]],
-  ["Lijiang Tower", new Set[""]],
+  ["Hollywood", new Set["D.va", "Doomfist"]],
+  ["Illios", new Set["Doomfist", "Orisa", "Roadhog", "Ramattra"]],
+  ["Junkertown", new Set["Wrecking Ball", "Doomfist"]],
+  ["King's Row", new Set["Zarya", "Mauga", "D.va"]],
+  ["Lijiang Tower", new Set["Reindhart", "Mauga", "Doomfist"]],
   ["Midtown", new Set[""]],
-  ["Nepal", new Set[""]],
+  ["Nepal", new Set["Doomfist"]],
   ["New Junk City", new Set[""]],
   ["New Queen Street", new Set[""]],
-  ["Numbani", new Set[""]],
-  ["Oasis", new Set[""]],
-  ["Paraíso", new Set[""]],
-  ["Rialto", new Set[""]],
-  ["Route 66", new Set[""]],
+  ["Numbani", new Set["D.va", "Doomfist"]],
+  ["Oasis", new Set["Mauga", "Junker Queen"]],
+  ["Paraíso", new Set["D.va", "Doomfist"]],
+  ["Rialto", new Set["Doomfist"]],
+  ["Route 66", new Set["D.va", "Doomfist"]],
   ["Runasapi", new Set[""]],
-  ["Samoa", new Set[""]],
+  ["Samoa", new Set["Mauga", "Junker Queen"]],
   ["Shambali Monastery", new Set[""]],
-  ["Suravasa", new Set[""]],
-  ["Throne of Anubis", new Set[""]],
-  ["Watchpoint: Gibraltar", new Set[""]]
+  ["Suravasa", new Set["Junker Queen", "Mauga"]],
+  ["Throne of Anubis", new Set["Mauga", "Junker Queen"]],
+  ["Watchpoint: Gibraltar", new Set["D.va", "Doomfist", "Winston", "Wrecking Ball"]]
 ])
 
 const maps_damage = new Map ([
